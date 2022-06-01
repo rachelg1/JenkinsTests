11d573b690b6851f808ddfc7d85e8593f0dc618b
rgreenstein <rgreenstein@liveperson.com>
2022-06-01 14:59:41 +0300

    schedule

---
+++ src/seed_job.groovy
@@ -0,0 +1,43 @@
+import hudson.model.*
+import hudson.security.*
+import groovy.json.JsonSlurper
+
+String config_file = readFileFromWorkspace("src/projects.json")
+
+def InputJSON = new JsonSlurper().parseText(config_file);
+
+InputJSON.projects.each {
+    if (it.group != null && it.service_name != null && it.enable == true) {
+        createPipeline(it)
+    }
+    return true
+}
+
+def createPipeline(service) {
+
+    def addr = "https://toga.int.liveperson.net/api/groups/${service.group}"
+    def conn = addr.toURL().openConnection()
+    def feed = conn.content.text
+    def results = new JsonSlurper().parseText(feed.toString())
+
+    def newJobName = "NFT_${service.service_name}_critical_pipeline"
+    if (!service.in_container_user?.trim()) {
+        service.in_container_user = "root"
+    }
+    pipelineJob(newJobName) {
+
+        definition {
+            cps {
+                script(readFileFromWorkspace('src/main_pipeline.groovy'))
+            }
+        }
+
+        parameters {
+            wHideParameterDefinition {
+                name("SERVICE_DETAILS")
+                description("service details")
+                defaultValue(service)
+            }
+        }
+    }
+}
