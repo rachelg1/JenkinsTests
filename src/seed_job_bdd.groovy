a12527c5706b884c3f08604c27c711fe8d782ae7
rachelg1 <45752429+rachelg1@users.noreply.github.com>
2022-06-01 14:38:16 +0300

    Update seed_job_bdd.groovy

--- src/seed_job_bdd.groovy
+++ src/seed_job_bdd.groovy
@@ -30,47 +30,15 @@ def createPipeline(service) {
         definition {
             cps {
                 script(
-                node (){
-    def currentStage = "";
-    def String[] scenarios_arr
-    def failed_scenarios = []
-    scenarios_arr = "s1,s2,s3".split(',')
-    scenarios_arr.each { entry ->
+                "node() {
 
-        //catchError(message: "scenario : $entry - failed", buildResult: 'FAILURE', stageResult: 'FAILURE') {
-        stage(entry) {
-            try {
-                echo "$entry"
-                currentStage = entry
-                if (entry == "s2")
-                    throw new Exception("hello")
-            }
-            //}
-            catch (e) {
-                echo 'scenario : ' + entry + ' - failed: ' + e
-                failed_scenarios.add(entry)
-                // Since we're catching the exception in order to report on it,
-                // we need to re-throw it, to ensure that the build is marked as failed
-                //throw e
-            }
+    stage(\'critical_dependencies\') {
+        Integer A_END = System.currentTimeMillis()/1000
+        def A_START = A_END - (3 * 60)
+        println(A_END)
+        println(A_START)
         }
-
-    }
-
-    if (failed_scenarios.size() != 0) {
-        echo 'failed_scenarios: ' + "[\"${failed_scenarios.join('", "')}\"]"
-        currentBuild.result = 'FAILURE'
-    }
-
-}
-
-def runBehaveBDD(scenario, A_START, A_END) {
-    sh '''set -x
-echo "set up variable for the shell"
-    scenario="''' + scenario + '''"
-'''
-
-}
+}"
                 )
 
             }
